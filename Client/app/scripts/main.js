1c2769a1d3f7e6590c751a93d2acd4d613bece40
Ada <adriannarys94@gmail.com>
2016-06-04 00:45:24 +0200

    more web components

--- Client/app/scripts/main.js
+++ Client/app/scripts/main.js
@@ -6,11 +6,70 @@ var MyComponent = Vue.extend({
 // register
 // Vue.component('my-component', MyComponent)
 
+
+var UserArea = Vue.extend({
+  template: `
+  <nav class="navbar navbar-default navbar-fixed-top navigation">
+    <div class="container-fluid">
+      <a class="navbar-brand"><img class="" src="images/logo.png">   </a>
+      <ul v-if="loggedin" class="nav navbar-nav navbar-right action-list">
+        <ul class="nav pull-right user-actions">
+          <li class="dropdown" id="menuSignUp">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navSignUp">Sign Up</a>
+            <div class="dropdown-menu signup-modal" style="padding:17px;">
+              <form class="form" id="formSignUp">
+                <input name="username" id="username" type="text" placeholder="Username">
+                <input name="password" id="password" type="password" placeholder="Password"><br>
+                <button type="button" id="btnLogin" class="btn">Sign Up</button>
+              </form>
+            </div>
+          </li>
+          <li class="dropdown" id="menuLogin">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Login</a>
+            <div class="dropdown-menu login-modal" style="padding:17px;">
+              <form class="form" id="formLogin">
+                <input name="username" id="username" type="text" placeholder="Username">
+                <input name="password" id="password" type="password" placeholder="Password"><br>
+                <button type="button" id="btnLogin" class="btn">Login</button>
+              </form>
+            </div>
+          </li>
+        </ul>
+      </ul>
+
+      <ul v-if="!loggedin" class="nav navbar-nav navbar-right action-list">
+        <li>
+          <a href="{{ userProfile }}" data-toggle="dropdown" id="navSignUp">{{ name }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  `,
+  data: function() {
+    return {
+      name: 'Adusia',
+      userProfile: '#'
+    }
+  },
+  methods: {
+    displayName: function(){
+
+    }
+  }
+});
+
 var ImageFeed = Vue.extend({
   template: `
-  <ul id="">
+  <ul id="feed-list">
     <li v-for="image in images">
-      <image-card v-bind:src="image.src"></image-card>
+      <div class="thumbnail">
+        <image-card v-bind:src="image.src"></image-card>
+          <div class="caption">
+            <h3>{{ image.title }}</h3>
+            <p><a v-bind:href="image.authorProfile">{{ image.author }}</a></p>
+            <p class="views">{{ image.views }}</p>
+          </div>
+      </div>
     </li>
   </ul>
   `,
@@ -18,10 +77,18 @@ var ImageFeed = Vue.extend({
     return {
       images: [
         {
-          src: "https://placekitten.com/g/200/300"
+          src: "https://placekitten.com/g/200/300",
+          title: "#kicius",
+          author: "Adusia",
+          authorProfile: "#",
+          views: 4
         },
         {
-          src: "https://placekitten.com/g/200/300"
+          src: "https://placekitten.com/g/200/200",
+          title: "#kicius",
+          author: "Adusia",
+          authorProfile: "#",
+          views: 4
         }
       ]
     }
@@ -38,6 +105,7 @@ var ImageCard = Vue.extend({
 // register
 Vue.component('image-card', ImageCard);
 Vue.component('image-feed', ImageFeed);
+Vue.component('user-area', UserArea);
 // create a root instance
 new Vue({
   el: 'body'
