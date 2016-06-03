4d9e2d0a4ee69201fa497f829273742727961d00
fifarafka <wojtasikmonikia@gmail.com>
2016-06-03 20:09:34 +0200

    StatisticsRepository Interface added

---
+++ Statistics/StatService/StatService/Repositories/IStatisticsRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StatService.Model;
+
+namespace StatService.Repositories
+{
+    public interface IStatisticsRepository
+    {
+        List<Statistic> fingAll();
+        int Add(Statistic statistic);
+        Statistic findOne(int id);
+        Statistic Update(Statistic statistic);
+        bool Delete(int id);
+    }
+}
