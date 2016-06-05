605c389dfe0534e7207f374873c0900803a248c5
fifarafka <wojtasikmonikia@gmail.com>
2016-06-05 21:33:18 +0200

    Statistic edit

--- StatisticService/StatisticService/Model/Statistic.cs
+++ StatisticService/StatisticService/Model/Statistic.cs
@@ -8,10 +8,18 @@ namespace StatisticService.Model
 {
     public class Statistic
     {
-        public int Id { get; set; }
+        public int Id { get; }
         public String ImageId { get; set; }
         public DateTime ViewDate { get; set; }
         public IPAddress UserIp { get; set; }
         public int UserId { get; set; }
-    }
+
+        public Statistic(String imageId, DateTime viewDate, IPAddress userIp, int userId)
+        {
+            this.ImageId = imageId;
+            this.ViewDate = viewDate;
+            this.UserIp = userIp;
+            this.UserId = UserId;
+        }
+    }   
 }
