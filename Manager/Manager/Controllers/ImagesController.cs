30c2778e2e70425055313a6cd3bbf308c9c9a008
Rafał Malin <malin603@gmail.com>
2016-06-05 10:34:59 +0200

    Add logging support for images controller.

--- Manager/Manager/Controllers/ImagesController.cs
+++ Manager/Manager/Controllers/ImagesController.cs
@@ -8,28 +8,33 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using Manager.GraphicRepository;
+using Manager.LoggingService;
 
 namespace Manager.Controllers
 {
     public class ImagesController : ApiController
     {
         private GraphicRepositoryClient _repo = new GraphicRepositoryClient();
+        private LoggerServiceClient log = new LoggerServiceClient();
 
         // GET: api/Images/?limit=10
         public IEnumerable<Graphic> GetNewestImages([FromUri] int limit)
         {
+            log.addLog($"ImagesController: GET was called - GET: api/Images/?limit={limit}", LogLevel.INFO);
             return _repo.GetNewestImages(limit);
         }
 
         // GET: api/Images/?userId=10&imageId=abc
         public Graphic GetAuthorsImage([FromUri] ImageAndItsAuthor img)
         {
+            log.addLog($"ImagesController: GET was called - GET: api/Images/?userId={img.userId}&imageId={img.imageId}", LogLevel.INFO);
             return _repo.GetUserImages(img.userId).First(i => i.Id == img.imageId);          
         }
 
         // GET: api/Images/?userId=10
         public IEnumerable<Graphic> GetAuthorImages([FromUri] int userId)
         {
+            log.addLog($"ImagesController: GET was called - GET: api/Images/?userId={userId}", LogLevel.INFO);
             return _repo.GetUserImages(userId);
         }
 
@@ -37,7 +42,8 @@ namespace Manager.Controllers
         // POST: api/Images
         [Authorize]
         public HttpResponseMessage PostFormData()
-        {         
+        {
+            log.addLog($"ImagesController: GET was called - GET: api/Images", LogLevel.INFO);
             var httpRequest = HttpContext.Current.Request;
             if (httpRequest.Files.Count > 0)
             {
@@ -50,11 +56,14 @@ namespace Manager.Controllers
                         ImageStream = httpRequest.Files[0].InputStream
                     };
                     _repo.AddImage(imgToAdd);
+                    log.addLog("ImagesController: new image added to the repository: " +
+                               $"Id={imgToAdd.Id} , Title={imgToAdd.Title}, Author={imgToAdd.Author}", LogLevel.INFO);
                 }
 
                 return Request.CreateResponse(HttpStatusCode.Created);
             }
 
+            log.addLog("ImagesController: POST was called - GET: api/Images , but there was no files in HTTP request.", LogLevel.ERROR);
             return Request.CreateResponse(HttpStatusCode.BadRequest);
         }
 
