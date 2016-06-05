4117c212a95bd840f525df92ec8169bb7e252f8f
Ada <adriannarys94@gmail.com>
2016-06-05 17:54:54 +0200

    Fix fix fix fix.

--- Manager/Manager/App_Start/IdentityConfig.cs
+++ Manager/Manager/App_Start/IdentityConfig.cs
@@ -28,11 +28,11 @@ namespace Manager
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
+                RequiredLength = 4,
+             
+              
                 RequireLowercase = true,
-                RequireUppercase = true,
+           
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
--- Manager/Manager/App_Start/Startup.Auth.cs
+++ Manager/Manager/App_Start/Startup.Auth.cs
@@ -22,6 +22,8 @@ namespace Manager
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            
+
             // Configure the db context and user manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
